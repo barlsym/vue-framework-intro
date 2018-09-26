ac4928e597b3e71e223e842562727459fdc98633
Hasan DAG <hasan.dg@hotmail.com>
2018-09-26 11:14:17 +0300

    my first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/blog">Blog</router-link>
+    <router-link to="/account">Account</router-link>
+   <router-view></router-view>
+    <p>This is a test.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
---
+++ src/components/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h3>The Blog</h3>
+    <p>This is The Blog</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Blog"
+  }
+</script>
+
+<style scoped>
+
+</style>
